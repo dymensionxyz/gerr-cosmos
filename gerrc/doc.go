531d70dca4ea9b67039783f870171900296c1819
danwt <30197399+danwt@users.noreply.github.com>
2024-06-19 13:23:06 +0100

    doc

--- gerrc/doc.go
+++ gerrc/doc.go
@@ -1,2 +1,2 @@
-// Package gerrc extends the canonical google API design guideline errors with cosmos sdk registration
+// Package gerrc extends the canonical google API design guideline errors with cosmos sdk registration.
 package gerrc
